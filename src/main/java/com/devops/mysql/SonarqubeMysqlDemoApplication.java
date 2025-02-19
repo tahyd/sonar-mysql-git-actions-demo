c12b4ae959f70f44ce18c1465ee7702b74dfdcd0
tahyd <ta.hyd.003@gmail.com>
2025-02-19 15:40:30 +0530

    endpoint added

--- src/main/java/com/devops/mysql/SonarqubeMysqlDemoApplication.java
+++ src/main/java/com/devops/mysql/SonarqubeMysqlDemoApplication.java
@@ -9,10 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SonarqubeMysqlDemoApplication {
-    
+    @GetMapping
+	public String index() {
+		return "Hello World !";
+	}
 	private static final Logger LOGGER = LoggerFactory.getLogger(SonarqubeMysqlDemoApplication.class);
 	  public Environment getEnv() {
 		return env;
